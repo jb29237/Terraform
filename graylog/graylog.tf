56f10e12cd84993713078d1f235b46b945af5e68
jb29237 <jameswbrookins@gmail.com>
2018-10-29 23:09:23 -0400

    Added new project for standalone graylog instance.

---
+++ graylog/graylog.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "graylog" {
+  ami                         = "${var.ami}"
+  instance_type               = "${var.size}"
+  key_name                    = "${aws_key_pair.mainkey.key_name}"
+  vpc_security_group_ids      = ["${aws_security_group.secgroup1.id}"]
+  subnet_id                   = "${aws_subnet.main.id}"
+  associate_public_ip_address = true
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.vpc_subnet}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "${var.subnet_one}"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "secgroup1" {
+  name        = "graylog"
+  description = "Some default ports for graylog"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = "${var.myip}"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = "${var.myip}"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_route" "public_4" {
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = "${aws_subnet.main.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_key_pair" "mainkey" {
+  key_name   = "mainkey-pub"
+  public_key = "${var.pub_key}"
+}
---
+++ graylog/variables.tf
@@ -0,0 +1,30 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-9887c6e7"
+}
+
+variable "size" {
+  default = "t2.micro"
+}
+
+variable "vpc_subnet" {
+  default = "192.168.0.0/16"
+}
+
+variable "subnet_one" {
+  default = "192.168.20.0/24"
+}
+
+variable "myip" {
+  type = "list"
+}
+
+variable "pub_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPKVR4kSCqbDY1H1GzMw4v+fSYzq5lHAoMo9VWCLZaYkhw25AyuB/0WRMAkfGBPziTwfMhTM5Zv170u3Q+rq4+zJ7e3oxSEkENcU+9Y25cPKBttT5WSFwpUpBT1SKeT2smwUKqZOuz1A+G2aAKqmNo2QboXv1NCd0d/uMVG4doJAJFYBjL9pFnuDS9msE7+kzDPIe0PBgnoInnz528E+JKqwwJGCUUvmjGGXO/dYTXTI9JC+p7U1yd3ilVCzXOMZ4ji3HnYqTVH9hM7D2i+X52O/zGOgzsAgrRj7TW1FQBaLWZ+JQxeHLiynyiRXey4bT6G98v6Hi0s0hWSA5QP/kp"
+}
